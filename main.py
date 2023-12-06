b6fa49b8bfc8561350adedde1fbc57224f9e1c11
Kaden Parker <131911150+KadenParker1@users.noreply.github.com>
2023-12-05 18:42:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 git lfs install
-git clone https://huggingface.co/deepset/roberta-base-squad2 #INSERT LINK 
+git clone https://huggingface.co/gpt2-large #INSERT LINK 
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 
 model_name = "deepset/roberta-base-squad2"
